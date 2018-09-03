14d819ffb8c3f62e90e141f80cd61a7f5e751ea5
viky dwi santoso <vickky@programmer.net>
2018-09-04 00:16:28 +0700

    build.sh: clang version going crazy >.<

--- build.sh
+++ build.sh
@@ -8,14 +8,15 @@ export KBUILD_BUILD_USER=vicatz
 export KBUILD_BUILD_HOST=Dalang
 export TOOL_CHAIN_PATH="/home/vicatz/toolchains/linaro/gcc-linaro-7.3.1-2018.05-x86_64_aarch64-linux-gnu/bin/aarch64-linux-gnu-"
 export CLANG_TCHAIN="/home/vicatz/toolchains/clang/DragonTC-8.0.0-20180830/bin/clang"
-export CLANG_VERSION="$(${CLANG_TCHAIN} --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')cut -d'(' -f1,4)"
+export CLANG_VERSION="$(${CLANG_TCHAIN} --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g')"
 export LD_LIBRARY_PATH="${TOOL_CHAIN_PATH}/../lib"
 export PATH=$PATH:${TOOL_CHAIN_PATH}
 export builddir="${kernel_dir}/mateng"
 #export modules_dir="zip/system/lib/modules"
 export ZIPPER_DIR="${kernel_dir}/zip"
 export ZIP_NAME="wayang™${V}_Oreo-Los_Aosp.zip"
-export IMAGE="arch/arm64/boot/Image.gz-dtb";
+export ZIMAGE DIR="arch/arm64/boot"
+export IMAGE="arch/arm64/boot/Image.gz";
 JOBS="-j8"
 cd $kernel_dir
 make clean && make mrproper
@@ -43,8 +44,10 @@ zipit () {
         echo -e "Build Succesful!";
     fi
     echo "**** Copying zImage ****"
-    cp arch/arm64/boot/Image.gz-dtb ${ZIPPER_DIR}/tools/
-    echo "**** Copying modules ****"
+    cp arch/arm64/boot/Image.gz ${ZIPPER_DIR}/tools/
+    echo "**** Copying treble & non .dtb ****"
+	cp arch/arm64/boot/dts/qcom/msm8953-qrd-sku3-mido-nontreble.dtb ${ZIPPER_DIR}/nontreble/
+	cp arch/arm64/boot/dts/qcom/msm8953-qrd-sku3-mido-treble.dtb ${ZIPPER_DIR}/treble/
 
     cd ${ZIPPER_DIR}/
     zip -r9 ${ZIP_NAME} * -x README ${ZIP_NAME}
